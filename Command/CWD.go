35451ba5a93554347ec6868e1abb8895c7895d34
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-06-09 23:36:41 +0800

    实现 绝对路径与相对路径的转换

--- Command/CWD.go
+++ Command/CWD.go
@@ -1,31 +1,31 @@
 package Command
 
 import (
-	"fmt"
 	"ftp_go/models"
-	"strings"
+	"os"
+	"path/filepath"
 )
 
 func HandleCWD(dialog *models.WorkSpace, arguments []string) []byte {
 	if len(arguments) < 1 {
 		return []byte("500 CWD command requires a directory argument.\r\n")
 	}
-	if arguments[0] == "/" {
-		dialog.Dir = "/"
-		return []byte("250 Directory successfully changed.\r\n")
+	var newDir string
+	if filepath.IsAbs(arguments[0]) {
+		// 如果是绝对路径，直接使用
+		newDir = arguments[0]
+	} else {
+		// 如果是相对路径，与当前工作目录进行合并
+		newDir = filepath.Join(dialog.Dir, arguments[0])
 	}
-	args := strings.Split("/"+arguments[0], "/")
-	dir := dialog.Dir
-	for _, str := range args {
-		if str == "" {
-			continue
-		}
-		dir = dir + str + "/"
+	// 检查新的工作目录是否存在
+	if _, err := os.Stat(filepath.Join(dialog.BasicDir, newDir)); os.IsNotExist(err) {
+		return []byte("550 No such directory." + newDir + "\r\n")
 	}
-	// 获取客户端传递的目录参数
-	fmt.Println("dir :", dir)
 
-	dialog.Dir = dir
+	// 更新工作目录
+	dialog.Dir = newDir
+
 	// 返回成功响应给客户端
 	return []byte("250 Directory successfully changed.\r\n")
 }
