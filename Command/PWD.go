87899384d1849707971c70161b14038880c233d1
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-06-09 23:37:10 +0800

    当前目录查看

---
+++ Command/PWD.go
@@ -0,0 +1,14 @@
+package Command
+
+import (
+	"fmt"
+	"ftp_go/models"
+)
+
+func HandlePWD(dialog *models.WorkSpace) []byte {
+	//todo 设置基准路径
+	// PWD 命令，返回当前工作目录路径
+	dir := dialog.Dir
+	response := fmt.Sprintf("257 \"%s\" is the current directory.\r\n", dir)
+	return []byte(response)
+}
