93c7fe2ef1c0825e3527268a3f2c1e29a1a2a518
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-06-10 16:09:25 +0800

    完整的RNTO

---
+++ Command/RNTO.go
@@ -0,0 +1,31 @@
+package Command
+
+import (
+	"ftp_go/models"
+	"os"
+	"path/filepath"
+)
+
+func HandleRNTO(dialog *models.WorkSpace, arguments []string) []byte {
+	// 检查是否已经执行过 RNFR 命令
+	if dialog.RNFR == "" {
+		return []byte("503 Bad sequence of commands.\r\n")
+	}
+
+	// 检查是否有权限重命名文件
+	if !checkPermissions(dialog, dialog.RNFR) {
+		return []byte("550 Permission denied.\r\n")
+	}
+
+	// 重命名文件
+	err := os.Rename(dialog.RNFR, filepath.Join(dialog.BasicDir, dialog.Dir, arguments[0]))
+	if err != nil {
+		return []byte("550 Failed to rename file.\r\n")
+	}
+
+	// 清空 RNFR
+	dialog.RNFR = ""
+
+	// 返回成功响应给客户端
+	return []byte("250 File renamed successfully.\r\n")
+}
